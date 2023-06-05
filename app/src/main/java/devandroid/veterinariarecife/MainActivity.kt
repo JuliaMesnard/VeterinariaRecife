00319d286916eb44a01718adfa8f7a79b122d0a8
Julia Mesnard <115434987+JuliaMesnard@users.noreply.github.com>
2023-06-05 20:02:23 -0300

    Update MainActivity.kt

--- app/src/main/java/devandroid/veterinariarecife/MainActivity.kt
+++ app/src/main/java/devandroid/veterinariarecife/MainActivity.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 import android.widget.Toast
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
-import devandroid.vinicius.veterinariarecife.databinding.ActivityMainBinding
+import devandroid.veterinariarecife.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
